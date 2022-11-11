b46f07f00d7d54e1858d4023d4006fc8b800386b
Bruno Alla <browniebroke@users.noreply.github.com>
2022-11-11 17:16:36 +0000

    Set PATH variables

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@ if ! command -v brew &> /dev/null
 then
   echo "Missing Homebrew, installing..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  # Make sure "brew" command in the PATH
+  eval $(/opt/homebrew/bin/brew shellenv)"
 fi
 
 if ! command -v ansible-playbook &> /dev/null

a42bcacdff6cc0c91009eb6c90da8dbcfe2a9abb
Bruno Alla <alla.brunoo@gmail.com>
2022-11-14 23:36:39 +0000

    Support several locations for Homebrew

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+# Make sure potential locations of brew are in the path
+export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
+
 if ! command -v brew &> /dev/null
 then
   echo "Missing Homebrew, installing..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  # Make sure "brew" command in the PATH
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+  # Set up brew environment
+  eval "$(brew shellenv)"
 fi
 
 if ! command -v ansible-playbook &> /dev/null
